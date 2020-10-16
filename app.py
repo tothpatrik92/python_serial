5512fb43763f555cfe2a77c0d8b4f8ce90550f25
Patrik Toth <tothpatrik92@gmail.com>
2020-10-16 15:25:44 +0200

    cmd buttons for GUI

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import time
 import threading
 import serial
 import string
-import tkinter as tk
+from tkinter import *
+
 
 thread_flag = None
 
-root = tk.Tk()
+root = Tk()
 root.title("Zigbee CLI")
-root.geometry("400x240")
+root.geometry("400x500")
 
 def getTextInput():
     result=textExample.get("1.0","end")
@@ -52,28 +53,37 @@ def Task1(ser):
         if thread_flag == 'stop': break
         else: thread_flag = 'paused'   # signals that the inner loop is done
 
+def nwk_steering():
+    
+    ser.write(b'plugin network-steering start 0\r\n')     # write a string
 
+def nwk_leave():
+    ser.write(b'network leave\r\n')     # write a string   
+    
+def nwk_info():
+    ser.write(b'info\r\n')     # write a string      
     
 ser = serial.Serial('COM7', 112500, timeout=0, parity=serial.PARITY_EVEN, rtscts=0)  
 t1 = threading.Thread(target = Task1, args=[ser])
 Report("Starting Thread 1")
 t1.start()
-textExample=tk.Text(root, height=10)
+textExample=Text(root, height=10)
 textExample.pack()
-btnRead=tk.Button(root, height=1, width=10, text="Send", command=getTextInput)
-btnRead.pack()
-
-#ser = serial.Serial(7, 11520)
-
-
-
+Button_Send=Button(root, height=1, width=10, text="Send", command=getTextInput)
+Button_Send.pack()
+Y=0.38
+DIST=0.1
+Button_NwkLeave= Button(root, text="Nwk Leave",command=nwk_steering)
+Button_NwkLeave.place(relx = 0.9, rely = Y, anchor = CENTER)
+Button_NwkSteering= Button(root, text="Nwk Steering",command=nwk_leave)
+Button_NwkSteering.place(relx = 0.9, rely = Y+DIST, anchor = CENTER)
+Button_NwkInfo= Button(root, text="Info",command=nwk_info)
+Button_NwkInfo.place(relx = 0.9, rely = Y+DIST+DIST, anchor = CENTER)
 
 
 #time.sleep(3)
 
-ser.write(b'info\r\n')     # write a string
-time.sleep(1)
-ser.write(b'network leave\r\n')     # write a string
-time.sleep(1)
-ser.write(b'plugin network-steering start 0\r\n')     # write a string
+#ser.write(b'info\r\n')     # write a string
+
+
 root.mainloop()
